7845a65ce811faaae554cc1b799d548870fe33e3
Gediminas <gediss.lolas78@gmail.com>
2025-03-25 18:17:26 +0200

    responsiveness increase

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Shop">
-    <div class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-center mx-auto  mb-10 p-16"> 
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 xl:grid-cols-4 lg:grid-cols-3 justify-center mx-auto  mb-10 p-16"> 
         @foreach ($products as $product)
             <x-product-card :product="$product" />
         @endforeach
