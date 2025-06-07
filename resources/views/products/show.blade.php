663be1f42c410cabc8e5532a2a059de1e150aada
Gediminas <gediss.lolas78@gmail.com>
2025-06-07 14:59:49 +0300

    breadcrumbs

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,3 +1,8 @@
 <x-layout title="{{ $product->name }}">
+    <flux:breadcrumbs class="max-w-[1210px] mx-auto mt-4">
+        <flux:breadcrumbs.item href="{{ route('home') }}">{{ __('Home') }}</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item href="{{ route('products') }}">{{ __('Shop') }}</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item>{{ $product->name }}</flux:breadcrumbs.item>
+    </flux:breadcrumbs>
     <livewire:add-to-cart :product="$product" />
 </x-layout>
